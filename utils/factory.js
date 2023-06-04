3c5f9c3fad9571b7dc4a4431e605d2b030ce343e
“Laman <hesenlileman2001@gmail.com>
2023-06-04 20:06:45 +0400

    Add headers for get method

--- utils/factory.js
+++ utils/factory.js
@@ -8,6 +8,9 @@ exports.getAll = (Model) =>
     query.filter().sort().fields().paginate();
     const doc = await query.query;
 
+    res.header("X-Total-Count", `${doc.length}`);
+    res.header("Access-Control-Expose-Headers", "Content-Range");
+
     res.status(200).json({
       status: "success",
       length: doc.length,
